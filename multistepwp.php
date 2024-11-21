28d5ebd6a0fb1c768ed365d78b11c8997bfd0f6c
Rahim_Badsa <rahimbadsa723@gmail.com>
2024-11-21 14:43:07 +0600

    creating multi step form wp

---
+++ multistepwp.php
@@ -0,0 +1,10 @@
+/**
+ * Plugin Name: Muti
+ * Description: Multi Step form WP
+ * Plugin URI: https://github.com/developerbadsa
+ * Author: Rahim Badsa
+ * Version: 0.0.0
+ * Author URI: https://www.linkedin.com/in/rahim-badsa/
+ * Text Domain: ms_fwp
+
+ */
